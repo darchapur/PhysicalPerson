a973eb44e40feae9d5da179d1a50365d163e155f
darchapur <148961478+darchapur@users.noreply.github.com>
2023-11-09 15:02:59 +0300

    Add files via upload

---
+++ ManageInformation/ManageInformation.Infrastructure/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ManageInformation.Infrastructure
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
