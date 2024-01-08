6372395b58040c3f799996c73bb33daaff804e71
Полина Фурман <miss.furman@mail.ru>
2024-01-09 01:01:39 +0300

    Кучу всего наделала, пробую закоммитить

--- ManageInformation/ManageInformation.API/Program.cs
+++ ManageInformation/ManageInformation.API/Program.cs
@@ -1,15 +1,23 @@
 using Microsoft.EntityFrameworkCore;
+using ManageInformation.Infrastructure.Interfaces;
+using ManageInformation.Infrastructure.Repos;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<MvdInterface, MvdRepo>();
+builder.Services.AddScoped<PfrInterface, PfrRepo>();
+builder.Services.AddScoped<PersonInterface, PersonRepo>();
+builder.Services.AddScoped<GibddInterface, GibddRepo>();
+builder.Services.AddScoped<NalogovayaInterface, NalogovayaRepo>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<ManageInformation.Infrastructure.Context>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+//Изменила на постгре
+builder.Services.AddDbContext<ManageInformation.Infrastructure.Data.Context>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -21,7 +29,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+//Новое
+app.UseDeveloperExceptionPage();
+//
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ ManageInformation/ManageInformation.Infrastructure/Interfaces/PersonInterface.cs
@@ -0,0 +1,20 @@
+﻿using ManageInformation.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageInformation.Infrastructure.Interfaces
+{
+    public interface PersonInterface
+    {
+        ICollection<Person> GetPersons();
+        Person GetPersonsById(int id);
+        bool CreatePerson(Person person);
+        bool UpdatePerson(Person person);
+        bool DeletePerson(Person person);
+        bool PersonExists(int id);
+        bool Save();
+    }
+}
